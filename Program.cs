99e690e279e37adc875f8ad6c0540e2a9d8334ae
silenkovkonstantin <silenkov@inbox.ru>
2023-02-01 21:25:07 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace App
+{
+    class Pen
+    {
+        public string color;
+        public int cost;
+
+        public Pen()
+        {
+            color = "Черный";
+            cost = 100;
+        }
+
+        public Pen(string penColor, int penCost)
+        {
+            color = penColor;
+            cost = penCost;
+        }
+    }
+}
